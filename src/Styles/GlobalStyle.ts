21e11e9495da2a34ab9ccc057fdfaa60a80a9349
kimjuno97 <jh241093@gmail.com>
2022-11-22 16:58:21 +0900

    feat: 초기세팅
    docs: 불필요한 파일 정리
    
    feat: react-router-dom, styled-components 추가및 메인 페이지 path지정

---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * {
+        box-sizing: border-box;
+    }
+    html {
+    font-size: 62.5%; /* 1rem 을 10px로 변환한 코드*/
+    }
+    body {
+        background-color: black;
+        color: white;
+        overflow-x: hidden;
+    }
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
+    li {
+    list-style: none;
+    }
+`;
+
+export default GlobalStyle;
