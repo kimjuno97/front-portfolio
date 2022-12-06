adbc0026289cc5808cb5c6d8141ef2dc97aa06ac
kimjuno97 <jh241093@gmail.com>
2022-12-06 13:44:47 +0900

    styling: parallax 미세 조정

--- src/pages/Projects/oneProject/FiveMillion.tsx
+++ src/pages/Projects/oneProject/FiveMillion.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import S from '../Styled.Projects';
 
-export default function FiveMillion() {
+export default function FiveMillion({ showBox }: { showBox: boolean }) {
 	return (
 		<a
 			href='https://github.com/kimjuno97/36-2nd-5MILLION-frontend'
 			target='_blank'
 			rel='noreferrer'>
-			<S.ProjectBox>
+			<S.ProjectBox showBox={showBox}>
 				<S.ProjectName>FiveMillion</S.ProjectName>
 				<S.ProjectImg
 					src={`${process.env.PUBLIC_URL}/img/fivemillion.gif`}
--- src/pages/Projects/oneProject/Ussop.tsx
+++ src/pages/Projects/oneProject/Ussop.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import S from '../Styled.Projects';
 
-export default function Ussop() {
+export default function Ussop({ showBox }: { showBox: boolean }) {
 	return (
 		<a
 			href='https://github.com/kimjuno97/36-1st-Usopp-frontend'
 			target='_blank'
 			rel='noreferrer'>
-			<S.ProjectBox>
-				<S.ProjectName>Todolist</S.ProjectName>
+			<S.ProjectBox showBox={showBox}>
+				<S.ProjectName>Ussop</S.ProjectName>
 				<S.ProjectImg
 					src={`${process.env.PUBLIC_URL}/img/ussop.gif`}
 					alt='todolist'
