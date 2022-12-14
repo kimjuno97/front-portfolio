e1824d6d1199bc93e6c83e93fc5bb14772417efd
kimjuno97 <jh241093@gmail.com>
2022-12-14 20:16:07 +0900

    15 puzzle game (#16)
    * add: 배포시 404page 추가(라우팅을 위해 추가)
    
    * add: puzzle 추가
    
    * fix: scale -> hoverScale

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './pages/Header/Header';
 import Profile from './pages/Profile/Profile';
+import Puzzle from './pages/Puzzle/Puzzle';
 
 export default function Roter() {
 	const [accordionToggle, setAccordionToggle] = useState(false);
@@ -12,8 +13,9 @@ export default function Roter() {
 				setAccordionToggle={setAccordionToggle}
 			/>
 			<Routes>
+				<Route path='/' element={<Puzzle />} />
 				<Route
-					path='/'
+					path='/main'
 					element={<Profile accordionToggle={accordionToggle} />}
 				/>
 			</Routes>
