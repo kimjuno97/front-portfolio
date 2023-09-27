97524b56c536399ece13c0a4712bd1153deb3528
rogan <jh241093@gmail.com>
2023-09-27 10:15:32 +0900

    refactor : profile 문장 수정 및 css 반응형 대응

--- src/pages/Profile/Styled.Profile.ts
+++ src/pages/Profile/Styled.Profile.ts
@@ -1,58 +1,69 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { TypeAccordionIcon } from '../../types';
+import { TypeAccordionIcon } from "../../types";
 
 const Container = styled.div`
-	margin-top: 8rem;
-	padding: 6rem 0;
-	background: #2750e5c9;
+  margin-top: 8rem;
+  padding: 6rem 0;
+  background: #2750e5c9;
+  min-height: 46rem;
+  @media (max-width: 700px) {
+    min-height: 70rem;
+  }
 `;
 const ProfileContainer = styled.div`
-	display: flex;
-	justify-content: space-around;
-	@media (max-width: 700px) {
-		flex-direction: column-reverse;
-		gap: 3rem;
-	}
+  display: flex;
+  justify-content: space-around;
+  @media (max-width: 820px) {
+    flex-direction: column-reverse;
+    gap: 3rem;
+  }
 `;
 
 const ProfileTextArea = styled.div`
-	display: flex;
-	min-width: 37rem;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  @media (min-width: 700px) {
+    min-width: 50rem;
+    max-width: 50rem;
+  }
+
+  @media (max-width: 820px) {
+    padding-left: 1rem;
+  }
 `;
 
 const ProfileText = styled.div<TypeAccordionIcon>`
-	width: auto;
-	min-height: 2rem;
-	color: white;
-	font-size: 2.2rem;
-	line-height: 3rem;
-	font-weight: bold;
-	transition: 1s;
-	opacity: ${({ accordionToggle }) => (accordionToggle ? '0' : '1')};
+  width: auto;
+  min-height: 2rem;
+  color: white;
+  font-size: 2.2rem;
+  line-height: 3rem;
+  font-weight: bold;
+  transition: 1s;
+  white-space: pre-wrap;
+  opacity: ${({ accordionToggle }) => (accordionToggle ? "0" : "1")};
 `;
 
 const Profile = styled.img`
-	width: 25rem;
-	height: 25rem;
-	border-radius: 9999px;
-	z-index: 1;
-	@media (max-width: 700px) {
-		margin-left: 50%;
-		transform: translate(-50%, 0);
-	}
+  width: 25rem;
+  height: 25rem;
+  border-radius: 9999px;
+  z-index: 1;
+  @media (max-width: 820px) {
+    margin-left: 50%;
+    transform: translate(-50%, 0);
+  }
 `;
 
 const S = {
-	Container,
-	ProfileContainer,
-	ProfileTextArea,
-	ProfileText,
-	Profile,
+  Container,
+  ProfileContainer,
+  ProfileTextArea,
+  ProfileText,
+  Profile,
 };
 
 export default S;
--- src/pages/Profile/useProfile.tsx
+++ src/pages/Profile/useProfile.tsx
@@ -1,35 +1,42 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from "react";
 
 export default function useProfile() {
-	const [text, setText] = useState({
-		top: '',
-		bot: '',
-	});
-	const countRef = useRef({
-		top: -1,
-		bot: -1,
-	});
-	useEffect(() => {
-		setTimeout(() => {
-			if (text.top !== PROFILE_TOP) {
-				countRef.current.top += 1;
-				setText(prev => ({
-					...prev,
-					top: PROFILE_TOP.substring(0, countRef.current.top),
-				}));
-			}
-			if (text.top === PROFILE_TOP && text.bot !== PROFILE_BOT) {
-				countRef.current.bot += 1;
-				setText(prev => ({
-					...prev,
-					bot: PROFILE_BOT.substring(0, countRef.current.bot),
-				}));
-			}
-		}, 100);
-	}, [text]);
-	return { text };
+  const [text, setText] = useState({
+    top: "",
+    bot: "",
+  });
+  const countRef = useRef({
+    top: -1,
+    bot: -1,
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      if (text.top !== PROFILE_TOP) {
+        countRef.current.top += 1;
+        setText((prev) => ({
+          ...prev,
+          top: PROFILE_TOP.substring(0, countRef.current.top),
+        }));
+      }
+      if (text.top === PROFILE_TOP && text.bot !== PROFILE_BOT) {
+        countRef.current.bot += 1;
+        setText((prev) => ({
+          ...prev,
+          bot: PROFILE_BOT.substring(0, countRef.current.bot),
+        }));
+      }
+    }, 100);
+  }, [text]);
+  return { text };
 }
 
-const PROFILE_TOP = '인터랙티브한 경험을 나누는';
+const PROFILE_TOP = `안녕하세요, 
+소프트웨어 개발자 김준호입니다. 
+저는 서비스 개발을 할 때 
+'만 원 내고 쓸 수 있는 서비스인가'를 
+늘 고민을 하고 개선합니다.`;
 
-const PROFILE_BOT = '프론트엔드 개발자 김준호입니다';
+const PROFILE_BOT = `개발자 포지션에서 
+먼저 만 원 내고 쓸 수 있는 서비스를 만들면,
+다른 업종의 전문가와 협업을 하면 
+그 가치는 충분히 만 배 이상 띌 것이라고 생각합니다.`;
