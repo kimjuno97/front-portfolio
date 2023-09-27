08ba2c7b8086c307d58fb5343440116a1fcbf5b9
rogan <jh241093@gmail.com>
2023-09-27 11:07:53 +0900

    refactor : About me blog, github주소 변경

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,74 +1,83 @@
-import S from './Styled.AboutMe';
+import S from "./Styled.AboutMe";
 
-import { BsGithub } from 'react-icons/bs';
-import { MdEmail } from 'react-icons/md';
-import { IoMdCall } from 'react-icons/io';
-import { FaBloggerB } from 'react-icons/fa';
+import { BsGithub } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { FaBloggerB } from "react-icons/fa";
 
-import useAboutMe from './useAboutMe';
+import useAboutMe from "./useAboutMe";
+
+interface AboutMeProperty {
+  url: string;
+  text: string;
+}
+
+const BLOG: AboutMeProperty = {
+  url: "https://kimjunho97.tistory.com",
+  text: "Juno's Technical blog",
+};
+
+const GITHUB: AboutMeProperty = {
+  url: "https://github.com/kimjuno97",
+  text: "Rogan",
+};
 
 export default function AboutMe() {
-	const { parallax, observerRef } = useAboutMe();
+  const { parallax, observerRef } = useAboutMe();
 
-	return (
-		<S.AboutMeContainer id='about' ref={observerRef}>
-			<S.AboutMeTitle parallax={parallax}>☎︎ ABOUT ME</S.AboutMeTitle>
-			<FaBloggerB
-				size='4rem'
-				style={{
-					gridArea: 'vIcon',
-					transition: '2s',
-					opacity: parallax >= 95 ? 1 : 0,
-					transform: parallax > 95 ? 'translateY(0%)' : 'translateY(50%)',
-				}}
-			/>
-			<S.AbuotText
-				href='https://velog.io/@juno97'
-				target='_blank'
-				gridArea='vText'
-				parallax={parallax}>
-				juno97.log
-			</S.AbuotText>
-			<BsGithub
-				size='4rem'
-				style={{
-					gridArea: 'gIcon',
-					transition: '2s',
-					opacity: parallax >= 95 ? 1 : 0,
-					transform: parallax > 95 ? 'translateY(0%)' : 'translateY(50%)',
-				}}
-			/>
-			<S.AbuotText
-				href='https://github.com/kimjuno97'
-				target='_blank'
-				gridArea='gText'
-				parallax={parallax}>
-				kimjuno97
-			</S.AbuotText>
-			<IoMdCall
-				size='4rem'
-				style={{
-					gridArea: 'pIcon',
-					transition: '2s',
-					opacity: parallax >= 95 ? 1 : 0,
-					transform: parallax > 95 ? 'translateY(0%)' : 'translateY(50%)',
-				}}
-			/>
-			<S.AbuotText gridArea='pText' parallax={parallax}>
-				010-4558-6895
-			</S.AbuotText>
-			<MdEmail
-				size='4rem'
-				style={{
-					gridArea: 'eIcon',
-					transition: '2s',
-					opacity: parallax >= 95 ? 1 : 0,
-					transform: parallax > 95 ? 'translateY(0%)' : 'translateY(50%)',
-				}}
-			/>
-			<S.AbuotText gridArea='eText' parallax={parallax}>
-				jh241093@gmail.com
-			</S.AbuotText>
-		</S.AboutMeContainer>
-	);
+  const commonIconCssProperty: React.CSSProperties = {
+    transition: "2s",
+    opacity: parallax >= 95 ? 1 : 0,
+    transform: parallax > 95 ? "translateY(0%)" : "translateY(50%)",
+  };
+
+  return (
+    <S.AboutMeContainer id="about" ref={observerRef}>
+      <S.AboutMeTitle parallax={parallax}>☎︎ ABOUT ME</S.AboutMeTitle>
+      <a href={BLOG.url} target="_blank" rel="noreferrer">
+        <FaBloggerB
+          size="4rem"
+          style={{
+            gridArea: "vIcon",
+            ...commonIconCssProperty,
+          }}
+        />
+      </a>
+      <S.AboutText
+        href={BLOG.url}
+        target="_blank"
+        gridArea="vText"
+        parallax={parallax}
+      >
+        {BLOG.text}
+      </S.AboutText>
+      <a href={GITHUB.url} target="_blank" rel="noreferrer">
+        <BsGithub
+          size="4rem"
+          style={{
+            gridArea: "gIcon",
+            ...commonIconCssProperty,
+          }}
+        />
+      </a>
+      <S.AboutText
+        href={GITHUB.url}
+        target="_blank"
+        gridArea="gText"
+        parallax={parallax}
+      >
+        {GITHUB.text}
+      </S.AboutText>
+
+      <MdEmail
+        size="4rem"
+        style={{
+          gridArea: "eIcon",
+          ...commonIconCssProperty,
+        }}
+      />
+      <S.AboutText gridArea="eText" parallax={parallax}>
+        jh241093@gmail.com
+      </S.AboutText>
+    </S.AboutMeContainer>
+  );
 }
--- src/pages/AboutMe/Styled.AboutMe.ts
+++ src/pages/AboutMe/Styled.AboutMe.ts
@@ -1,51 +1,50 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { TypeParallax, TypeGrid } from '../../types';
+import { TypeParallax, TypeGrid } from "../../types";
 
 const AboutMeContainer = styled.div`
-	display: grid;
-	grid-template-areas:
-		'header header'
-		'vIcon vText'
-		'gIcon gText'
-		'pIcon pText'
-		'eIcon eText';
-	grid-template-columns: 5rem 30rem;
-	align-items: center;
-	column-gap: 5rem;
-	row-gap: 4rem;
-	min-width: 30rem;
-	height: 100vh;
-	margin: 0 auto;
-	padding: 12rem 6rem;
-	background: tomato;
+  display: grid;
+  grid-template-areas:
+    "header header"
+    "vIcon vText"
+    "gIcon gText"
+    "eIcon eText";
+  grid-template-columns: 5rem 30rem;
+  align-items: center;
+  column-gap: 5rem;
+  row-gap: 4rem;
+  min-width: 30rem;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 12rem 6rem;
+  background: tomato;
 `;
 
 const AboutMeTitle = styled.h1<TypeParallax>`
-	grid-area: header;
-	font-size: 5rem;
-	font-weight: bold;
-	transition: 0.5s;
-	opacity: ${({ parallax }) => parallax / 100};
-	transform: ${({ parallax }) =>
-		parallax <= 85 ? `translateX(-${150 - parallax}%)` : `translateX(0%)`};
+  grid-area: header;
+  font-size: 5rem;
+  font-weight: bold;
+  transition: 0.5s;
+  opacity: ${({ parallax }) => parallax / 100};
+  transform: ${({ parallax }) =>
+    parallax <= 85 ? `translateX(-${150 - parallax}%)` : `translateX(0%)`};
 `;
 
 const AboutContents = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 3rem;
+  display: flex;
+  align-items: center;
+  gap: 3rem;
 `;
 
-const AbuotText = styled.a<TypeGrid>`
-	grid-area: ${({ gridArea }) => gridArea};
-	font-size: 3rem;
-	transition: 2s;
-	opacity: ${({ parallax }) => (parallax >= 95 ? 1 : 0)};
-	transform: ${({ parallax }) =>
-		parallax >= 95 ? 'translateY(0%)' : 'translateY(50%)'};
+const AboutText = styled.a<TypeGrid>`
+  grid-area: ${({ gridArea }) => gridArea};
+  font-size: 3rem;
+  transition: 2s;
+  opacity: ${({ parallax }) => (parallax >= 95 ? 1 : 0)};
+  transform: ${({ parallax }) =>
+    parallax >= 95 ? "translateY(0%)" : "translateY(50%)"};
 `;
 
-const S = { AboutMeContainer, AboutMeTitle, AboutContents, AbuotText };
+const S = { AboutMeContainer, AboutMeTitle, AboutContents, AboutText };
 
 export default S;
